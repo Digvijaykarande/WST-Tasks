0072360f919e57d5656e7303c5b811ad68886d67
Digvijaykarande <karandedigvijay2@gmail.com>
2026-04-21 20:57:01 +0530

    wst new tasks

---
+++ WST_lab_tasks/file.js
@@ -0,0 +1,38 @@
+// const fs=require('fs')
+
+// fs.readFile('test.txt','utf8',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     else
+//     console.log(data);
+    
+// })
+// console.log("file reading started....")
+
+// let names=['john','maria','chris']
+
+// console.log(names)
+
+// let nums=[1,52,23,3,5]
+
+// console.log(nums.reverse());
+
+// console.log(nums.sort((a,b)=>a-b))
+ 
+
+// let student={name:"digvijay",id:28};
+// student.id=2
+// console.log(student)
+
+
+let employee={
+    name:"digvijay",
+    empID:28,
+    salary:50000,
+    age:22,
+    skills:["React",'springboot'],
+    dateJoining: new Date("2026-12-13")
+}
+
+console.log(employee)
---
+++ WST_lab_tasks/sum.js
@@ -0,0 +1,22 @@
+// let a=10;
+// let b=20;
+// let sum=a+b;
+// function display(){
+//     console.log("The sum of a and b is: "+sum);
+// }display();
+
+
+// function show(){
+//     for (let i=1; i<=10; i++){
+//     console.log(i);
+// }
+// }
+
+// show();
+
+
+console.log("start");
+setTimeout(()=>{
+    console.log("timeout")
+},0)
+console.log("End")
