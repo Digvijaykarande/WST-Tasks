d4d464de4ca23b98a5d9232d78de6960ca066275
digvijay karande <151751999+Digvijaykarande@users.noreply.github.com>
2026-04-21 20:51:52 +0530

    Delete WS_Tech_JS_Lab_28 directory

--- WS_Tech_JS_Lab_28/event.js
+++
@@ -1,5 +0,0 @@
-let myButton=document.getElementById("myButton");
-
-myButton.addEventListener("click", function() {
-    document.getElementById("message").textContent = "Button was clicked successfully!";
-});
